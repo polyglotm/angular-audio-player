be2be057f123ee119a5cfa19416a3d4ec941e029
polyglot.m <polyglot.m@gmail.com>
2016-07-25 00:32:18 +0900

    chore(example): update example
    Former-commit-id: 6bba4302d30953fc2195697e74bbb1c7bb3b7015

--- src/app/intro/intro.route.js
+++ src/app/intro/intro.route.js
@@ -1,11 +1,15 @@
 'use strict';
 
+import IntroController from './intro.controller';
+
 export default function routerConfig ($stateProvider, $urlRouterProvider) {
   'ngInject';
 
   $stateProvider
     .state('home.intro', {
       url: 'intro',
-      templateUrl: 'app/intro/intro.html'
+      templateUrl: 'app/intro/intro.html',
+      controller: IntroController,
+      controllerAs: 'introVm'
     });
 }
