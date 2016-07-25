9c8e2bebb62eddb2067f510aa30e017c44187a10
polyglot.m@gmail.com <polyglot.m@gmail.com>
2016-07-25 23:03:19 +0900

    chore(style.js): css extention not support
    Former-commit-id: dabcd04b2dcf7e486772a5ae525583fc2c68cf31

--- gulp/styles.js
+++ gulp/styles.js
@@ -27,7 +27,6 @@ var buildStyles = function() {
   };
 
   var injectFiles = gulp.src([
-    path.join(conf.paths.src, '/app/**/*.css'),
     path.join(conf.paths.src, '/app/**/*.scss'),
     path.join('!' + conf.paths.src, '/app/index.scss'),
     path.join('!' + conf.paths.src, '/app/scss-modules')
