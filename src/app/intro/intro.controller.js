e942e78b8fa14f5e8929bfa0169944b0b521f235
polyglot.m@gmail.com <polyglot.m@gmail.com>
2016-07-27 00:01:58 +0900

    feat(intro): update intro

--- src/app/intro/intro.controller.js
+++ src/app/intro/intro.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 export default class IntroController {
-  constructor() {
+  constructor(S3_AUDIO_LIST) {
+    'ngInject';
+
+    this.audio = {
+      audioSrc : 'assets/audios/Pastel-Shade-01-Longing-displace-320k.mp3',
+      audioS3Src : S3_AUDIO_LIST.s1
+    }
   }
 }
