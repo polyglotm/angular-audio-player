5578c2bf9bd12dbb484420055e83b2768377e3c2
polyglot.m@gmail.com <polyglot.m@gmail.com>
2016-07-26 23:55:22 +0900

    feat(example): update example
    Former-commit-id: e8ee8734d4041784637d8f7ca2543d3fa421ec2c

--- src/app/example-1/example-1.controller.js
+++ src/app/example-1/example-1.controller.js
@@ -4,11 +4,20 @@ export default class Example1Controller {
   constructor() {
     this.exampleAudio1 = {
       audioSrc: 'assets/audios/Pastel-Shade-01-if-you-can-stop-that`s-not-love-320k.mp3',
-      audioFilename: 'Pastel-Shade-01-if-you-can-stop-that`s-not-love-320k.mp3'
+      audioFilename: 'Pastel-Shade-01-if-you-can-stop-that`s-not-love-320k.mp3',
+      doc: 
+`<lc-audio-player 
+  audio-src="/input/your/src/url"
+  audio-filename="/input/your/file/name">
+</lc-audio-player>`
     }
 
     this.exampleAudio2 = {
-      audioSrc: 'assets/audios/EFIX & Kokenn - Louise (Feat Edgar).mp3'
+      audioSrc: 'assets/audios/EFIX & Kokenn - Louise (Feat Edgar).mp3',
+      doc:
+`<lc-audio-player 
+  audio-src="/input/your/src/url">
+</lc-audio-player>`
     }
 
     this.exampleAudio3 = {
@@ -16,7 +25,13 @@ export default class Example1Controller {
       audioFilename: 'uneasy-dream(a-bittersweet-life-ost).mp3',
       options: {
         tooltip: false
-      }
+      },
+      doc:
+`<lc-audio-player 
+  audio-src="/input/your/src/url"
+  audio-filename="/input/your/file/name"
+  options="{ tooltip : false }">
+</lc-audio-player>`
     }
   }
 }
